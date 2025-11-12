7c2f011652cd0f35f72e85fe732f719f9c3fbe7c
Huari29 <cristoteam29@gmail.com>
2025-11-12 15:58:10 -0400

    muchos cambios

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,7 +7,7 @@
             <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
                 <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
                 <Div>
-                    <table>
+                    <table class="border=1">
                         <thead>
                             <tr>
                                 <td>{{__('Nombre')}}</td>
